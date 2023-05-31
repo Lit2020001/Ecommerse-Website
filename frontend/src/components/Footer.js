9803042f376479341a7923badc224d4d31b86cc6
Prafful Vishnoi <lit2020001@iiitl.ac.in>
2023-05-31 22:10:37 +0530

    initial commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className='mt-auto text-center' id={classes.footer}>
+      &#169; Emart - Made With <i class='fa-solid fa-heart'></i> By Prafful Vishnoi
+    </div>
+  )
+}
+
+export default Footer
